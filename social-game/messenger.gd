d0500e060d38f9e1e222a998a7174bc35d030327
AdamCMunro <adammunro5609@gmail.com>
2026-04-30 13:42:21 +0100

    added typing mechanic

--- social-game/messenger.gd
+++ social-game/messenger.gd
@@ -1,6 +1,13 @@
 extends Node2D
 
 @onready var viewport_centre = get_viewport_rect().size / 2
+@onready var option1 = $MessengerBody/Option1
+@onready var option2 = $MessengerBody/Option2
+@onready var option3 = $MessengerBody/Option3
+@onready var option_arr = [option1, option2, option3]
+@onready var sending_text = $MessengerBody/SendingText
+
+var typing = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,3 +17,40 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _option_chosen() -> void:
+	
+	for option in option_arr:
+		var option_tween = create_tween()
+		
+		option_tween.tween_property(option, "position", Vector2(option.position.x, option.position.y - 15), 0.15)\
+			.set_ease(Tween.EASE_IN_OUT)\
+			.set_trans(Tween.TRANS_SINE)
+		option_tween.tween_property(option, "position", Vector2(option.position.x, option.position.y + 500), 0.1)\
+			.set_ease(Tween.EASE_IN_OUT)\
+			.set_trans(Tween.TRANS_SINE)
+		
+		await option_tween.finished
+		
+	sending_text.visible = true
+	sending_text.visible_ratio = 0
+	_show_sending_text()
+	typing = true
+
+
+func _input(event: InputEvent):
+	if event is InputEventKey and event.pressed:
+		if typing:
+			if sending_text.text == "Type random letters to send":
+				sending_text.visible_ratio = 0
+				sending_text.text = option1.full_text
+			else:
+				sending_text.visible_characters += 1
+
+func _show_sending_text():
+	var tween = create_tween()
+	
+	tween.tween_property(sending_text, "visible_ratio", 1, 0.2)\
+		.set_ease(Tween.EASE_IN_OUT)\
+		.set_trans(Tween.TRANS_SINE)
--- social-game/option_1.gd
+++ social-game/option_1.gd
@@ -1,7 +1,12 @@
 extends Area2D
 
+signal option1_chosen
+
 @onready var selection = get_parent().get_node("Selection")
 @onready var messenger_body = get_parent()
+
+var option_text = "Option 1..."
+var full_text = "OMG YAAAAASSSSS QUEEN"
 
 var hovered = false
 var selected = false
@@ -30,17 +35,35 @@ func _on_mouse_exited() -> void:
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.is_pressed() && not event.is_echo():
-			_select()
-			messenger_body.get_node("Option2")._deselect()
-			messenger_body.get_node("Option3")._deselect()
+			if not selected:
+				_select()
+			else:
+				option1_chosen.emit()
 
 func _select():
 	selected = true
 	selection.visible = false
 	$ColorRect.set_color(white)
 	$OptionLabel.set("theme_override_colors/font_color",black)
+	messenger_body.get_node("Option2")._deselect()
+	messenger_body.get_node("Option3")._deselect()
+	_transition_text("Click again to confirm")
 
 func _deselect():
 	selected = false
 	$ColorRect.set_color(black)
 	$OptionLabel.set("theme_override_colors/font_color",white)
+	$OptionLabel.text = option_text
+	
+func _transition_text(new_text):
+	var tween = create_tween()
+	
+	tween.tween_property($OptionLabel, "visible_ratio", 0.0, 0.1)
+	
+	await tween.finished
+	
+	var tween2 = create_tween()
+	
+	$OptionLabel.text = new_text
+	tween2.tween_property($OptionLabel, "visible_ratio", 1.0, 0.2)
+	
