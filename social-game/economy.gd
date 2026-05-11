3d16c46f5b1fe6a7b26c81184d18590ce4bcca5d
AdamCMunro <adammunro5609@gmail.com>
2026-05-11 19:22:25 +0100

    transition between income and outgoing scenes added

--- social-game/economy.gd
+++ social-game/economy.gd
@@ -7,8 +7,12 @@ extends Node2D
 
 var green = "#6bff6a"
 var red = "#d0316c"
+var black = '#000000'
 
 var balance
+
+var button_hovered = false
+var button_pressed = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -22,6 +26,8 @@ func _ready() -> void:
 	$IncomeBox.position.y -= 150
 	$Income.position = viewport_centre
 	$Income.position.y -= 250
+	$Button.position = viewport_centre
+	$Button.position.y += 300
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -30,6 +36,14 @@ func _process(delta: float) -> void:
 		_update_label($Money/MoneyLabel, balance, red)
 	else:
 		_update_label($Money/MoneyLabel, balance, green)
+		
+	if button_hovered or button_pressed:
+		$Button/ColorRect.visible = true
+		$Button/Label.text = str("[color=", black, "]Continue[/color]")
+	else:
+		$Button/ColorRect.visible = false
+		$Button/Label.text = "Continue"
+
 	
 func _purchase(value):
 	_update_balance(player.money, player.money - value)
@@ -118,5 +132,59 @@ func _get_player_info():
 
 func _get_change_label_scene():
 	change_label_scene = preload("res://change_label.tscn")
+		
+func _on_button_mouse_entered() -> void:
+	button_hovered = true
+	$Button/ColorRect.visible = true
+	$Button/Label.text = str("[color=", black, "]Continue[/color]")
+
+
+func _on_button_mouse_exited() -> void:
+	button_hovered = false
+	$Button/ColorRect.visible = false
+	$Button/Label.text = "Continue"
+
+
+func _on_button_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.is_pressed() && not event.is_echo():
+			if not button_pressed:
+				button_pressed = true
+				_animate_button_press()
+				_move_to_buyables()
+
+func _animate_button_press():
+	var tween = create_tween()
+	var starting_scale = $Button.scale
+	var starting_pos = $Button.position
 	
+	tween.set_ease(Tween.EASE_IN_OUT)
+	tween.set_trans(Tween.TRANS_SINE)
+	
+	tween.tween_property($Button, "scale", starting_scale * 0.85, 0.15)
+	tween.tween_property($Button, "scale", starting_scale * 0.9, 0.2)
+	
+func _move_to_buyables():
+	var tween = create_tween()
+	
+	tween.set_ease(Tween.EASE_IN_OUT)
+	tween.set_trans(Tween.TRANS_SINE)
+	
+	tween.tween_property($Button, "position:y", 300, 0.2).as_relative()
+	
+	tween.tween_property($Income, "position:y", 40, 0.1).as_relative()
+	tween.parallel().tween_property($IncomeBox, "position:y", 40, 0.1).as_relative()
+	tween.parallel().tween_property($Money, "position:y", 40, 0.1).as_relative()
+		
+	tween.tween_property($Income, "position:y", -475, 0.3).as_relative()
+	tween.parallel().tween_property($IncomeBox, "position:y", -475, 0.3).as_relative()
+	tween.parallel().tween_property($Money, "position:y", -475, 0.3).as_relative()
+	
+	tween.tween_property($Income, "position:y", 30, 0.2).as_relative()
+	tween.parallel().tween_property($IncomeBox, "position:y", 30, 0.2).as_relative()
+	tween.parallel().tween_property($Money, "position:y", 30, 0.2).as_relative()
+	
+	tween.tween_property($BuyableBox, "position:y", -600, 0.4).as_relative()
+	tween.tween_property($BuyableBox, "position:y", 15, 0.2).as_relative()
+
 	
